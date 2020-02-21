f30a79a8eacf05e81ad881a184d257f08e5b2153
SurajMondem <surajmondem@gmail.com>
2020-02-21 07:28:26 -0800

    Updates

---
+++ precache-manifest.48bd5366756b9b7a6bc20b696c8ebbd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee099eda754a1779ef0b93946d1eb3a",
+    "url": "/Portfolio-website/index.html"
+  },
+  {
+    "revision": "704665b16e2499308078",
+    "url": "/Portfolio-website/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "1c6cf78399bb05237f6b",
+    "url": "/Portfolio-website/static/css/main.1d75ae06.chunk.css"
+  },
+  {
+    "revision": "704665b16e2499308078",
+    "url": "/Portfolio-website/static/js/2.3909d67e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Portfolio-website/static/js/2.3909d67e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c6cf78399bb05237f6b",
+    "url": "/Portfolio-website/static/js/main.54a338e3.chunk.js"
+  },
+  {
+    "revision": "f74326d52fe52ed6d15f",
+    "url": "/Portfolio-website/static/js/runtime-main.158149e5.js"
+  }
+]);
