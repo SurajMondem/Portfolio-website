72ade01dded702c8b19744bce3c6cccc1e650f36
SurajMondem <surajmondem@gmail.com>
2020-10-29 17:04:17 -0400

    Landing page Revamp

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import Conclusion from "./Components/Conclusion/Conclusion";
 import SideDrawer from "./Components/SideDrawer/SideDrawer";
 import Backdrop from "./Components/Backdrop/Backdrop";
 import Skills from "./Components/Skills/Skills";
-import Background from "./Assets/Images/Zion-park.jpg"
+import Background from "./Assets/Images/Zion-park.jpg";
+import LandingSection from "./Components/LandingSection/LandingSection"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 class App extends Component {
@@ -30,9 +32,9 @@ class App extends Component {
 
         let backdrop;
 
-        const sectionStyle = {
-            backgroundImage: "url(" + {Background} + ")"
-        };
+        // const sectionStyle = {
+        //     backgroundImage: "url(" + {Background} + ")"
+        // };
 
         if(this.state.sideDrawerOpen) {
              backdrop = <Backdrop click={this.backdropClickHandler}/>
@@ -43,16 +45,11 @@ class App extends Component {
                 <Navigation drawerClickHandler = {this.drawerToggleClickHandler} />
                 <SideDrawer show={this.state.sideDrawerOpen} />
                 {backdrop}
-                <main style={{marginTop: '62px'}}>
-                    <div style={sectionStyle} className={"big-picture"}>
-                        <svg className={"canvas"} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#273036" fillOpacity= "1" d="M0,192L720,320L1440,160L1440,320L720,320L0,320Z"> </path>
-                        </svg>
-                    </div>
-                    <Skills/>
-                    <Projects/>
-                    <Aboutme/>
-                    <Conclusion/>
-                </main>
+                <LandingSection/>
+                <Skills/>
+                <Projects/>
+                <Aboutme/>
+                <Conclusion/>
             </div>
         );
     }
--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,37 +1,112 @@
-import React from "react";
+import React, { Component } from "react";
+import classnames from "classnames";
 import './Navigation.css';
 import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
+import Backdrop from '../Backdrop/Backdrop'
 
 const Resumeurl = 'SurajMondem_Resume.pdf';
-const toolbar = props => (
-    <header className={"toolbar"}>
-        <nav className={"toolbar_navigation"}>
-            <div className={"toolbar_toggle-button"}>
-                <DrawerToggleButton click={props.drawerClickHandler }/>
-            </div>
-            <div className={"toolbar_logo"}><a href="/">SURAJ MONDEM</a></div>
-            <div className={"spacer"}></div>
-            <div className={"toolbar_navigation_items"}>
-                <ul>
-                    <li>
-                        <a href={"#aboutme"}>About Me</a>
-                    </li>
-                    <li>
-                        <a href={Resumeurl} target={'_blank'}>Resume</a>
-                    </li>
-                    <li>
-                        <a href={"#portfolio"}>Projects</a>
-                    </li>
-                    <li>
-                        <a href={"/"}>Photography</a>
-                    </li>
-                    <li>
-                        <a href={"#contact-me"}>Contact Me</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
-);
 
-export default toolbar;
+class Navigation extends Component {
+    constructor(){
+        super()
+        this.state = {
+            prevScrollpos: window.pageYOffset,
+            visible: false,
+        }
+    }
+    
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
+    }
+
+    // Remove the event listener when the component is unmount.
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+
+    handleScroll = () => {
+        const { prevScrollpos } = this.state;
+
+        const currentScrollPos = window.pageYOffset;
+        const visible = prevScrollpos > currentScrollPos;
+
+        this.setState({
+            prevScrollpos: currentScrollPos,
+            visible
+        });
+    };
+    
+    render(){
+        return(
+            <React.Fragment>
+                <nav className={
+                    classnames(
+                        "nav-bar", 
+                            {
+                                "scroll": !this.state.visible
+                            }
+                        )
+                }>
+                <div className={"toolbar_toggle-button"}>
+                    <DrawerToggleButton click={this.props.drawerClickHandler }/>
+                </div>
+                <header className={"navbar-logo"}>
+                    <h3><a href="/">SURAJ MONDEM</a></h3>
+                </header>
+                <div className={"spacer"}></div>
+                <div className={"navbar-items"}>
+                    <ul>
+                        <li>
+                            <a href={"#aboutme"}>About Me</a>
+                        </li>
+                        <li>
+                            <a href={Resumeurl} target={'_blank'}>Resume</a>
+                        </li>
+                        <li>
+                            <a href={"#portfolio"}>Projects</a>
+                        </li>
+                        <li>
+                            <a href={"/"}>Photography</a>
+                        </li>
+                        <li>
+                            <a href={"#contact-me"}>Contact Me</a>
+                        </li>
+                    </ul>
+                </div>
+                </nav>
+            </React.Fragment>
+        )
+    }
+}
+
+// const Navigations = props => (
+
+//     // <header className={"toolbar"}>
+//     //     <nav className={"toolbar_navigation"}>
+//     //         
+//     //         <div className={"toolbar_logo"}><a href="/">SURAJ MONDEM</a></div>
+//     //         <div className={"spacer"}></div>
+//     //         <div className={"toolbar_navigation_items"}>
+//     //             <ul>
+//     //                 <li>
+//     //                     <a href={"#aboutme"}>About Me</a>
+//     //                 </li>
+//     //                 <li>
+//     //                     <a href={Resumeurl} target={'_blank'}>Resume</a>
+//     //                 </li>
+//     //                 <li>
+//     //                     <a href={"#portfolio"}>Projects</a>
+//     //                 </li>
+//     //                 <li>
+//     //                     <a href={"/"}>Photography</a>
+//     //                 </li>
+//     //                 <li>
+//     //                     <a href={"#contact-me"}>Contact Me</a>
+//     //                 </li>
+//     //             </ul>
+//     //         </div>
+//     //     </nav>
+//     // </header>
+// );
+
+export default Navigation;
