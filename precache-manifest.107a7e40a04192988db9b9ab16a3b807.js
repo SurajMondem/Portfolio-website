ca164dc801b4057317dc6c5d45408eea0202d6a2
SurajMondem <51679986+SurajMondem@users.noreply.github.com>
2020-12-27 19:33:20 +0000

    Deploying to gh-pages from  @ d2feafe71067d182a60f7c4b58c11c254e662f6b 🚀

---
+++ precache-manifest.107a7e40a04192988db9b9ab16a3b807.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ec9c1d8746bc85d6806ec74f5bce20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9d246ad1ade06f6b11b",
+    "url": "/static/css/2.2d63d0b4.chunk.css"
+  },
+  {
+    "revision": "ee4024973c8a7f8c105f",
+    "url": "/static/css/main.ef4b9715.chunk.css"
+  },
+  {
+    "revision": "c9d246ad1ade06f6b11b",
+    "url": "/static/js/2.d0fcb7ec.chunk.js"
+  },
+  {
+    "revision": "3f4d2f3c6ab3995d06da96c7441c8c6e",
+    "url": "/static/js/2.d0fcb7ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee4024973c8a7f8c105f",
+    "url": "/static/js/main.0c2235b9.chunk.js"
+  },
+  {
+    "revision": "9b41c7e13f690904baac",
+    "url": "/static/js/runtime-main.c3e8af0c.js"
+  },
+  {
+    "revision": "52de9a09bbf2d22d24f364fcf0310d02",
+    "url": "/static/media/Mern.52de9a09.jfif"
+  },
+  {
+    "revision": "38c3a911493bf29320caa2cb901e0ecb",
+    "url": "/static/media/Node2.38c3a911.png"
+  },
+  {
+    "revision": "c9907aa0d4e53b669c37b3b935b493b0",
+    "url": "/static/media/Photoshop.c9907aa0.png"
+  },
+  {
+    "revision": "a777aeb7889d8fa490427a74d8246429",
+    "url": "/static/media/Portfolio_pic.a777aeb7.jpg"
+  },
+  {
+    "revision": "7c0fdc490914ad5e54763db4ef955066",
+    "url": "/static/media/React.7c0fdc49.png"
+  },
+  {
+    "revision": "2d0ef7ea2f9270a181df6e14eac79262",
+    "url": "/static/media/java-logo.2d0ef7ea.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d8d3d3dd8d911028af5714e6aee0410a.js"
+  "/precache-manifest.107a7e40a04192988db9b9ab16a3b807.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
